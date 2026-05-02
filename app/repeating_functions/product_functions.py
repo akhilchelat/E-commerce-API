8a89d28ab80cfaa335a2495165db8ef79dbf6b8c
Akhil Chelat <chelatakhilc@gmail.com>
2026-05-02 19:30:56 +0530

    created the create order service function

--- app/repeating_functions/product_functions.py
+++ app/repeating_functions/product_functions.py
@@ -12,6 +12,10 @@ def find_product(db: Session, product_id: int):
     return product
 
 def find_products(db: Session, product_ids: list[int]):
+
+    if not product_ids:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="No products provided")
+    
     
     products = (db.query(Product).filter(Product.id.in_(product_ids), 
                                         Product.is_active.is_(True))
@@ -24,3 +28,16 @@ def find_products(db: Session, product_ids: list[int]):
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Some products are invalid")
     
     return products
+
+def total_price_of_products(db: Session, product_ids: list[int]):
+
+    if not product_ids:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="No products provided")
+    
+    total_price = (db.query(sum(Product.price)).filter(Product.id.in_(product_ids),
+                                                       Product.is_active.is_(True)).first())
+    
+    if not total_price:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No price found")
+    
+    return total_price
--- app/services/order_service.py
+++ app/services/order_service.py
@@ -1,4 +1,5 @@
 from app.models.order import Order
+from app.models.orderitem import OrderItem
 from app.models.cartitem import CartItem
 from sqlalchemy.orm import Session
 from sqlalchemy import func
@@ -6,13 +7,52 @@ from fastapi import HTTPException, status
 from sqlalchemy.exc import IntegrityError
 from app.repeating_functions.user_functions import find_user
 from app.repeating_functions.cart_functions import get_selected_cart_items
+from app.repeating_functions.product_functions import total_price_of_products
 
 
-def create_order(db: Session, user_id: int, cart_item_id: list[int]):
+def create_order(db: Session, user_id: int, cart_item_ids: list[int]):
 
     find_user(db, user_id)
     
-    cart_items = get_selected_cart_items(db, user_id, cart_item_id)
+    cart_items = get_selected_cart_items(db, user_id, cart_item_ids)
+
+    total_price = 0
+    
+    for item in cart_items:
+        if item.product.stock < item.quantity:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"product is out of stock {item.product_id}")
+        
+        total_price += item.product.price * item.quantity
+
+    
+    try:
+        new_order = Order(user_id=user_id, total_price=total_price)  
+    
+        db.add(new_order)    
+        db.flush()
+
+        for item in cart_items:
+            order_item = OrderItem(order_id=new_order.id, 
+                                   product_id=item.product_id, 
+                                   quantity=item.quantity, 
+                                   price=item.product.price)
+
+            db.add(order_item)
+
+            item.product.stock -= item.quantity
+
+        db.commit()
+        db.refresh(new_order)
+        
+        return new_order
+            
+    except IntegrityError:
+        db.rollback()
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="order already exist")
+
+    
+
+
 
      
 
